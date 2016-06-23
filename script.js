6bba2b34dade792732b26d148937b33592c7e3ed
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-06-23 18:01:01 -0400

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// var aladin = A.aladin('#aladin-lite-div', {
-//     target: 'helix',
-//     // survey: "", //find survey name
-//     fov: 1
-// });
-
 var aladin = A.aladin('#aladin-lite-div', {target: 'Crab', fov: 50});
 
-aladin.addCatalog(A.catalogFromVizieR('J/ApJS/218/23/table4','Crab', 10, {onClick: 'showTable'}));
+var catalog = A.catalogFromVizieR(
+  'J/ApJS/218/23/table4',
+  'Crab',
+  10,
+  {
+    onClick: 'showTable'
+  }
+);
+
+aladin.addCatalog(catalog);
