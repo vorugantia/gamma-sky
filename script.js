9670b127bece8cb46f36aa57a39084f7f32fcecc
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-06-28 16:51:25 -0400

    add make.py

--- script.js
+++ script.js
@@ -1,25 +1,83 @@
-var aladin = A.aladin(
-  '#aladin-lite-div',
-  {
+// The old way:
+//
+//var aladin = A.aladin(
+//   '#aladin-lite-div',
+//   {
+//     fullScreen: true,
+//     showFullscreenControl: false,
+//     survey: 'P/Fermi/color',
+//     cooFrame: 'galactic',
+//     target: '0 +0',
+//     fov: 30
+//   }
+// );
+//
+// var catalog = A.catalogFromVizieR(
+//   'J/ApJS/218/23/table4',
+//   'galactic',
+//   180, // TODO: Make this load faster
+//   {
+//     onClick: 'showTable'
+//   },
+//   function() {
+//     aladin.addCatalog(catalog);
+//   }
+// );
+
+var map;
+var catalog;
+
+function show_map() {
+  console.log("This is a show_map");
+  map = A.aladin("#aladin-lite-div", {
     fullScreen: true,
     showFullscreenControl: false,
     survey: 'P/Fermi/color',
     cooFrame: 'galactic',
     target: '0 +0',
     fov: 30
-  }
-);
+  });
+  console.log(map);
+}
 
-var catalog = A.catalogFromVizieR(
-  'J/ApJS/218/23/table4',
-  '0 +0',
-  180, // TODO: Make this load faster
-  {
-    onClick: 'showTable'
-  }
-);
+var add_catalog = function(data) {
+  console.log("This is add_catalog");
+  catalog = data.data;
+  console.log(data);
+  console.log(catalog);
+  var cat = A.catalog();
+  map.addCatalog(cat);
 
-aladin.addCatalog(catalog);
+  for(var i=0; i < catalog.length; i++) {
+    console.log(i, catalog[i]);
+    var source = catalog[i];
+    var marker = A.marker(source.ra, source.dec);
+    cat.addSources([marker]);
+    console.log(marker);
+  }
+};
+
+var main = function() {
+  console.log("This is main");
+
+  show_map();
+
+  console.log("Fetching catalog data...");
+  window.setTimeout(
+    function() {
+      $.getJSON("data/data.json", add_catalog);
+    },
+    [5000]
+  );
+  console.log("After the getJSON call...");
+
+  // add_catalog();
+  //console.log("Adding catalog...");
+};
+$(document).ready(main);
+
+
+
 
 $(document).ready(function(){
     var timesClicked = 0;
