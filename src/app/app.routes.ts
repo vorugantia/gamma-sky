ab721094ca35c2b59e73e84d37a2d75ce23593d7
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-08-02 14:29:47 -0400

    add app.routes.ts (commented out for now)

---
+++ src/app/app.routes.ts
@@ -0,0 +1,25 @@
+// import {provideRouter, RouterConfig} from '@angular/router';
+//
+// const routes: RouterConfig = [
+//
+//   {
+//     path: 'map',
+//     component: MapViewComponent,
+//     name: 'MapView'
+//   },
+//   {
+//     path: 'cat',
+//     component: CatViewComponent,
+//     name: 'CatView'
+//   },
+//   {
+//     path: '',
+//     redirectTo: 'MapView', //or just '/map'
+//     pathMatch: 'full'
+//   }
+//
+// ];
+//
+// export const appRouterProviders = [
+//   provideRouter(routes)
+// ]
