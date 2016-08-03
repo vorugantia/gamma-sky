376a1082cbac87eb791af81acf0ec218bec81cdd
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-08-03 16:43:05 -0400

    add aladin-lite-div and fix buttons

--- src/app/widgets/map/map.component.ts
+++ src/app/widgets/map/map.component.ts
@@ -1,16 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+
+declare var A: any;
+declare var $: any;
 
 @Component({
   moduleId: module.id,
   selector: 'map',
   templateUrl: 'map.component.html',
-  styleUrls: ['map.component.css']
+  styleUrls: ['map.component.css'],
+  encapsulation: ViewEncapsulation.None  // This allows the MapComponent to define styles that can affect
+                                         // the whole application - what we need for #aladin-lite-div
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
+
+  private map;
+
+  onInitConfig() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.overflow = "hidden";
+    document.documentElement.style.overflow = "hidden";
+  }
+
+  onDestroyConfig() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.overflow = "auto";
+    document.documentElement.style.overflow = "auto";
+  }
+
+  showMap() {
+    this.map = A.aladin("#aladin-lite-div", {
+      fullScreen: true,
+      showFullscreenControl: false,
+      survey: "P/Fermi/color",
+      cooFrame: "galactic",
+      target: "0 +0",
+      fov: 180,
+      allowFullZoomout: true, //Hidden attribute
+      showShareControl: true, //Hidden attribute, not yet working
+      // showCatalog: false //Hidden attribute
+    })
+  }
 
   constructor() { }
 
   ngOnInit() {
+
+    console.log('aladin map OnInit');
+    this.onInitConfig();
+
+    this.showMap();
+
+
+
+
+  }
+
+  ngOnDestroy() {
+    console.log('aladin map OnDestroy');
+    this.onDestroyConfig();
+
   }
 
 }
