7a1180bb1980b2da063465297a3d50bafef87968
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-08-03 14:16:34 -0400

    add code for AboutButtonComponent

--- src/app/widgets/about-button/about-button.component.ts
+++ src/app/widgets/about-button/about-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   moduleId: module.id,
   selector: 'about-button',
@@ -8,9 +10,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutButtonComponent implements OnInit {
 
+  private toggle: boolean;
+  private aboutButtonRight;
+  private aboutInfoRight;
+  private aboutInfoWidth;
+
+  onClick() {
+    $("#aboutInfo").toggle();
+
+    this.toggle = !this.toggle
+    this.changeButtonDisplay();
+  }
+
+  changeButtonDisplay() {
+    if(this.toggle == false) {
+      return "About";
+    }
+    else {
+      return "Close";
+    }
+  }
+
+  resize() {
+
+    var pageWidth = $(document).width();
+
+    if (pageWidth > 800) {
+      this.aboutButtonRight = "100px";
+      this.aboutInfoRight = "100px";
+    }
+    if (pageWidth <= 800) {
+      this.aboutButtonRight = "50px";
+      this.aboutInfoRight = "50px";
+
+    }
+    if (pageWidth > 500) {
+      this.aboutInfoWidth = "410px";
+    }
+    if (pageWidth <= 500) {
+      this.aboutInfoWidth = "205px";
+    }
+
+  }
+
   constructor() { }
 
   ngOnInit() {
+    this.toggle = false;
+    this.changeButtonDisplay();
+    this.resize();
   }
 
 }
