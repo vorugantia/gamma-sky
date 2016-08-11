b99506375e9dd3f4bb7c68f0d34525ce9c2569ec
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-08-11 06:39:02 -0400

    update surveys.ts with relevant CDS images

--- src/app/data/maps/surveys.ts
+++ src/app/data/maps/surveys.ts
@@ -25,6 +25,65 @@ export var SURVEYS: Survey[] = [
     "maxOrder": 3,
     "frame": "galactic",
     "format": "jpeg fits",
+  },
+  {
+    "id": "P/PLANCK/LFI/color",
+    "url": "http://alasky.u-strasbg.fr/PLANCK/R2/LFI_Color_30_44_70",
+    "name": "Planck R2 LFI Color 30-44-70 GHz",
+    "maxOrder": 3,
+    "frame": "galactic",
+    "format": "jpeg"
+  },
+  {
+    "id": "P/PLANCK/HFI/color",
+    "url": "http://alasky.u-strasbg.fr/PLANCK/HFIColor353-545-857",
+    "name": "Planck R1 HFI Color 353-545-857 GHz",
+    "maxOrder": 3,
+    "frame": "galactic",
+    "format": "jpeg"
+  },
+  {
+    "id": "P/PLANCK/R2/HFI/color",
+    "url": "http://alasky.u-strasbg.fr/PLANCK/R2/HFI_Color_353_545_857",
+    "name": "Planck R2 HFI Color 353-545-857 GHz",
+    "maxOrder": 3,
+    "frame": "galactic",
+    "format": "jpeg"
+  },
+  {
+    "id": "P/GLIMPSE360",
+    "url": "http://www.spitzer.caltech.edu/glimpse360/aladin/data",
+    "name": "GLIMPSE360",
+    "maxOrder": 9,
+    "frame": "equatorial",
+    "format": "jpeg"
+  },
+  {
+    "id": "P/IRIS/4",
+    "url": "http://cade.irap.omp.eu/documents/Ancillary/4Aladin/IRIS_4",
+    "name": "IRIS Band 4-100um",
+    "maxOrder": 3,
+    "frame": "galactic",
+    "format": "jpeg fits"
+  },
+  {
+    "id": "P/CGPS/VGPS/CONT",
+    "url": "http://cade.irap.omp.eu/documents/Ancillary/4Aladin/CGPS_VGPS_CONT/",
+    "name": "CGPS-VGPS CONT",
+    "maxOrder": 4,
+    "frame": "galactic",
+    "format": "jpeg fits"
+  },
+  {
+    "id": "P/AKARI/WideS",
+    "url": "http://cade.irap.omp.eu/documents/Ancillary/4Aladin/AKARI_WideS",
+    "name": "AKARI 90um",
+    "maxOrder": 3,
+    "frame": "galactic",
+    "format": "png jpeg fits"
   }
+
+
+
 
 ]
