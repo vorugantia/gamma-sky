c618a4fffab99f1795e17f3c2fb1941089bbf70e
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-10-26 20:39:56 -0400

    new angular-cli project

---
+++ README.md
@@ -0,0 +1,8 @@
+# gamma-sky.net
+
+A portal to the gamma-ray sky
+
+* Webpage: http://gamma-sky.net/
+* Repository: https://github.com/gammapy/gamma-sky
+* Documentation: https://github.com/gammapy/gamma-sky/wiki
+* [![Build Status](https://travis-ci.org/gammapy/gamma-sky.svg?branch=master)](https://travis-ci.org/gammapy/gamma-sky)
---
+++ make.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+"""Make gamma-sky.net input data.
+"""
+import click
+import gammasky
+
+@click.group()
+def cli():
+    """The gamma-sky.net Python cli"""
+    pass
+
+@cli.group()
+def cat():
+    """Dump catalog to JSON"""
+
+@cat.command('all')
+@click.option('--nrows', type=int, default=None)
+def cat_all(nrows):
+    """Dump TeV catalog to JSON"""
+    gammasky.make_tev_catalog_data(nrows=nrows)
+    gammasky.make_3fgl_catalog_data(nrows=nrows)
+    gammasky.make_2fhl_catalog_data(nrows=nrows)
+    gammasky.make_snrcat_catalog_data(nrows=nrows)
+
+@cat.command('tev')
+@click.option('--nrows', type=int, default=None)
+def cat_tev(nrows):
+    """Dump TeV catalog to JSON"""
+    gammasky.make_tev_catalog_data(nrows=nrows)
+
+@cat.command('3fgl')
+@click.option('--nrows', type=int, default=None)
+def cat_3fgl(nrows):
+    """Dump 3FGHL catalog to JSON"""
+    gammasky.make_3fgl_catalog_data(nrows=nrows)
+
+@cat.command('2fhl')
+@click.option('--nrows', type=int, default=None)
+def cat_2fhl(nrows):
+    """Dump 2FHL catalog to JSON"""
+    gammasky.make_2fhl_catalog_data(nrows=nrows)
+
+@cat.command('snrcat')
+@click.option('--nrows', type=int, default=None)
+def cat_snrcat(nrows):
+    """Dump SNRCat catalog to JSON"""
+    gammasky.make_snrcat_catalog_data(nrows=nrows)
+
+
+@cli.command()
+def maps():
+    """Make map data"""
+    gammasky.make_maps_data()
+
+if __name__ == '__main__':
+    cli()
