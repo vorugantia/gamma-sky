b827146c1d67b6d921b703a0eb77e32d77eb779d
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-10-28 19:08:04 -0400

    add aladin-lite-div

--- src/app/widgets/map/map.component.ts
+++ src/app/widgets/map/map.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// import {Popup} from '../popup/popup';
+
+// import {CatalogService} from '../../services/catalog.service';
+// import {SURVEYS} from '../../data/maps/surveys';
+
+declare var A: any;
+declare var HpxImageSurvey: any;
+declare var $: any;
 
 @Component({
   selector: 'map',
@@ -7,9 +15,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  private map;
+
+  //private error: any;
+
+  showMap() {
+    this.map = A.aladin("#aladin-lite-div", {
+      fullScreen: true,
+      showFullscreenControl: false,
+      survey: "P/Fermi/color",
+      cooFrame: "galactic",
+      target: "0 +0",
+      fov: 180,
+      allowFullZoomout: true, //Hidden attribute
+      showShareControl: true, //Hidden attribute, not yet working
+      // showCatalog: false //Hidden attribute
+    });
+  }
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log("aladin map onInit()");
+
+    this.showMap();
+
   }
 
 }
