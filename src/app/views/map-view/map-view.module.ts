f54218e9ac1f996c602e77c3b8d1f9de2530c303
Arjun Voruganti <arjun.voruganti@gmail.com>
2016-10-28 18:53:05 -0400

    add (empty) MapComponent

--- src/app/views/map-view/map-view.module.ts
+++ src/app/views/map-view/map-view.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 
 import { MapViewComponent } from './map-view.component';
+import { MapComponent } from '../../widgets/map/map.component';
 
 import { MapViewRoutingModule } from './map-view-routing.module';
 
 @NgModule({
   declarations: [
-    MapViewComponent
+    MapViewComponent,
+    MapComponent
   ],
   imports: [
     MapViewRoutingModule
