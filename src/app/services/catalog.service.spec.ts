ba64193743ebecd45720e3366fd1fb33905c682d
vorugantia <arjun.voruganti@gmail.com>
2017-07-11 23:22:23 +0200

    update

--- src/app/services/catalog.service.spec.ts
+++ src/app/services/catalog.service.spec.ts
@@ -1,12 +1,18 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import { HttpModule, Http } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { CatalogService } from './catalog.service';
 
 describe('Service: Catalog', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [CatalogService]
+      imports: [HttpModule],
+      providers: [
+        CatalogService,
+        {provide: Http, deps: [MockBackend]}
+      ]
     });
   });
 
