1581c551a299a99334a12b4f336a01cb7efdfcf2
vorugantia <arjun.voruganti@gmail.com>
2017-07-12 10:19:02 +0200

    improve make.py commands

--- make.py
+++ make.py
@@ -16,6 +16,11 @@ def cat():
     """Dump catalog to JSON"""
 
 
+@cli.group()
+def source():
+    """Dump source objects to JSON"""
+
+
 @cat.command('all')
 def cat_all():
     """Dump all catalogs to JSON"""
@@ -24,32 +29,56 @@ def cat_all():
     gammasky.make_3fgl_catalog_data()
     gammasky.make_snrcat_catalog_data()
 
+
 @cat.command('tev')
 def cat_tev():
     """Dump TeV catalog to JSON"""
     gammasky.make_tev_catalog_data()
+
 
 @cat.command('3fhl')
 def cat_tev():
     """Dump 3FHL catalog to JSON"""
     gammasky.make_3fhl_catalog_data()
 
+
 @cat.command('3fgl')
 def cat_3fgl():
     """Dump 3FGL catalog to JSON"""
     gammasky.make_3fgl_catalog_data()
+
 
 @cat.command('snrcat')
 def cat_snrcat():
     """Dump SNRCat catalog to JSON"""
     gammasky.make_snrcat_catalog_data()
 
-@cat.command('sources')
-def source_3fhl():
-    """TEST"""
+
+@source.command('all')
+def source_all():
+    """Dump all source objects to JSON"""
     gammasky.make_3fhl_source_data()
     gammasky.make_tev_source_data()
     gammasky.make_3fgl_source_data()
+
+
+@source.command('tev')
+def source_tev():
+    """Dump TeV source objects to JSON"""
+    gammasky.make_tev_source_data()
+
+
+@source.command('3fhl')
+def source_3fhl():
+    """Dump 3FHL source objects to JSON"""
+    gammasky.make_3fhl_source_data()
+
+
+@source.command('3fgl')
+def source_3fgl():
+    """Dump 3FGL source objects to JSON"""
+    gammasky.make_3fgl_source_data()
+
 
 @cli.command()
 def maps():
