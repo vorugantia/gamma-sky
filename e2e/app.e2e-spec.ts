e69c71df1b7117a75c4c6df3137e4ee721cf1657
vorugantia <arjun.voruganti@gmail.com>
2017-07-21 17:48:53 +0200

    update

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -10,32 +10,35 @@ describe('gamma-sky App', () => {
   it('should initialize the webpage', () => {
     page.navigateTo('/');
 
-    expect(page.getTextByTags('app-root h1')).toEqual('');
+    expect<any>(page.getTextByTags('app-root h1')).toEqual('');
   });
 
   it('should display tev source name in source detail view', () => {
-    page.navigateTo('/#/cat/tev/0');
+    page.navigateTo('/#/cat/tev/0')
+        .then(() => {
+          return expect<any>(page.getTextById('e2e-source-tev-name')).toEqual('CTA 1')
+    });
 
-    expect(page.getTextById('e2e-source-tev-name')).toEqual('CTA 1');
+
   });
 
   it('should display 3fhl source name in source detail view', () => {
     page.navigateTo('/#/cat/3fhl/0');
 
-    expect(page.getTextById('e2e-source-3fhl-name')).toEqual('3FHL J0001.2-0748');
+    expect<any>(page.getTextById('e2e-source-3fhl-name')).toEqual('3FHL J0001.2-0748');
   });
 
   it('should display 3fgl source name in source detail view', () => {
     page.navigateTo('/#/cat/3fgl/0');
 
-    expect(page.getTextById('e2e-source-3fgl-name')).toEqual('3FGL J0000.1+6545');
+    expect<any>(page.getTextById('e2e-source-3fgl-name')).toEqual('3FGL J0000.1+6545');
   });
 
   it('should click "About" button in Map View and read text', () => {
     page.navigateTo('/#/map');
 
     page.clickButton('About');
-    expect(page.getTextById('aboutInfo')).toContain('Gamma-sky.net');
+    expect<any>(page.getTextById('aboutInfo')).toContain('Gamma-sky.net');
   });
 
 });
