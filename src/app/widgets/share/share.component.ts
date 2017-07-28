766b29f3a903a784b8f5d3577ec09d408f695cac
vorugantia <arjun.voruganti@gmail.com>
2017-07-28 12:26:38 +0200

    update

--- src/app/widgets/share/share.component.ts
+++ src/app/widgets/share/share.component.ts
@@ -11,8 +11,6 @@ declare var $: any;
 })
 export class ShareButtonComponent implements OnInit {
 
-// Button to the left of "About". On click, a text field appears in the middle of the window. Share URL is inside. Also share URL is updated in website URL. The div for the field takes up the whole page, you can't click anything else. Then there's a "close" button below (make sure border + close button background are contrasting colors.) WIDTH 400px for field.
-
   private shareButtonRight;
   private urlString;
 
@@ -48,7 +46,7 @@ export class ShareButtonComponent implements OnInit {
 })
 export class ShareDialogComponent {
 
-  // Using https://stackoverflow.com/a/40733052/4726636
+  // See https://stackoverflow.com/a/40733052/4726636
   private urlString = this.document.location.href;
 
   constructor(@Inject(DOCUMENT) private document: any) {}
